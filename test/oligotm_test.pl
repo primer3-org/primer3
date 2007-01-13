856cfc4d2d09f3cd574f90b316a59b8276eb36e8
Steven G. Rozen <steve@steverozen.net>
2007-01-13 16:07:21 +0000

    Moved oligotm_test.pl to GPL

--- test/oligotm_test.pl
+++ test/oligotm_test.pl
@@ -1,3 +1,23 @@
+# Copyright (c) 2007
+# Triinu Koressaar an Maido Remm
+# All rights reserved.
+# 
+#     This file is part of the oligotm library.
+# 
+#     The oligotm library is free software; you can redistribute it and/or modify
+#     it under the terms of the GNU General Public License as published by
+#     the Free Software Foundation; either version 2 of the License, or
+#     (at your option) any later version.
+# 
+#     The oligotm library is distributed in the hope that it will be useful,
+#     but WITHOUT ANY WARRANTY; without even the implied warranty of
+#     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#     GNU General Public License for more details.
+# 
+#     You should have received a copy of the GNU General Public License
+#     along with the oligtm library (file gpl.txt in the source
+#     distribution); if not, write to the Free Software
+#     Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 
 use constant EPSILON => 1e-5;
 $nr=1;
