15387d64d1d9fd1ce580dc102a07ddfceec4aaa0
Steven G. Rozen <steve@steverozen.net>
2007-10-16 11:38:50 +0000

    continuing to make code more modular

--- read_boulder.c
+++ read_boulder.c
@@ -35,15 +35,20 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include <stdlib.h>  /* strtod, strtol,... */
 #include <ctype.h> /* toupper */
 #include <string.h> /* memset, strlen,  strcmp, ... */
+#include <unistd.h> /* write */
 #include "read_boulder.h"
 
 #define INIT_BUF_SIZE 1024
 #define INIT_LIB_SIZE  500
 #define PR_MAX_LIBRARY_WT 100.0
 
+/* Used in PR_ASSERT: */
 static char *pr_program_name = "Program name is probably primer3_core";
 
 /* Static functions. */
+static void *_rb_safe_malloc(size_t x);
+static void *_rb_safe_realloc(void *p, size_t x);
+
 static void   adjust_base_index_interval_list(interval_array_t, int, int);
 static void   parse_align_score(const char *, const char *, short *,
 				pr_append_str *);
@@ -85,7 +90,7 @@ extern double strtod();
                                "Duplicate tag: "); \
            pr_append(parse_err, TAG);              \
        } else {                                    \
-           T = pr_safe_malloc(datum_len + 1);      \
+           T = _rb_safe_malloc(datum_len + 1);      \
            strcpy(T, datum);                       \
        }                                           \
        continue;                                   \
@@ -311,7 +316,7 @@ read_record(const program_args *prog_args,  primer_args *pa, seq_args *sa)
 		    free(repeat_file);
 		    repeat_file = NULL;
 		} else {
-		    repeat_file = pr_safe_malloc(strlen(datum) + 1);
+		    repeat_file = _rb_safe_malloc(strlen(datum) + 1);
 		    strcpy(repeat_file, datum);
 		}
 		continue;
@@ -323,7 +328,7 @@ read_record(const program_args *prog_args,  primer_args *pa, seq_args *sa)
 		    free(int_repeat_file);
 		    int_repeat_file = NULL;
 		} else {
-		    int_repeat_file = pr_safe_malloc(strlen(datum) + 1);
+		    int_repeat_file = _rb_safe_malloc(strlen(datum) + 1);
 		    strcpy(int_repeat_file, datum);
 		}
 		continue;
@@ -538,7 +543,7 @@ FILE *file;
 
     if (NULL == s) {
 	ssz = INIT_BUF_SIZE;
-	s = pr_safe_malloc(ssz);
+	s = _rb_safe_malloc(ssz);
     }
     p = s;
     remaining_size = ssz;
@@ -566,7 +571,7 @@ FILE *file;
 	else {
 	    ssz *= 2;
 	}
-	s = pr_safe_realloc(s, ssz);
+	s = _rb_safe_realloc(s, ssz);
 	p = strchr(s, '\0');
 	remaining_size = ssz - (p - s);
     }
@@ -781,263 +786,6 @@ parse_product_size(tag_name, in, pa, err)
     pa->num_intervals = i;
 }
 
-#ifdef FOOBAR
-/*  
- * Reads any file in fasta format and fills in *lib.  Sets lib->error to a
- * non-empty string on error.
- */
-static void
-read_seq_lib(lib, filename, errfrag)
-    seq_lib *lib;
-    const char *filename;
-    const char *errfrag;
-{
-    char  *p;
-    FILE *file;
-    int i, m, k;
-    size_t j, n;
-    char buf[2];
-    char offender = '\0', tmp;
-
-    PR_ASSERT(NULL != lib);
-    PR_ASSERT(NULL != filename);
-
-    free_seq_lib(lib);
-
-    lib->repeat_file = pr_safe_malloc(strlen(filename) + 1);
-    strcpy(lib->repeat_file, filename);
-
-    if((file = fopen(lib->repeat_file,"r")) == NULL) {
-	pr_append_new_chunk(&lib->error,
-			    "Cannot open ");
-	goto ERROR;
-    }
-
-    j = INIT_BUF_SIZE;
-    n = INIT_LIB_SIZE;
-    lib->names = pr_safe_malloc(INIT_LIB_SIZE*sizeof(*lib->names));
-    lib->seqs  = pr_safe_malloc(INIT_LIB_SIZE*sizeof(*lib->seqs));
-    lib->weight= pr_safe_malloc(INIT_LIB_SIZE*sizeof(*lib->weight));
-    lib->seq_num = 0;
-
-    i = -1;  m = 0; k = 0;
-    while((p = read_line(file))) {
-	if(*p == '>'){
-	    i++;
-	    if(i >= n) {
-		n += INIT_LIB_SIZE;
-		lib->names = pr_safe_realloc(lib->names,n*sizeof(*lib->names));
-		lib->seqs  = pr_safe_realloc(lib->seqs ,n*sizeof(*lib->seqs));
-		lib->weight= pr_safe_realloc(lib->weight,
-					     n*sizeof(*lib->weight));
-	    }
-	    p++;
-	    lib->names[i] = pr_safe_malloc(strlen(p) + 1);
-	    strcpy(lib->names[i],p);
-	    lib->weight[i] = parse_seq_name(lib->names[i]);
-	    lib->seqs[i] = pr_safe_malloc(INIT_BUF_SIZE);
-	    lib->seqs[i][0] = '\0';
-	    lib->seq_num = i+1;
-	    if(lib->weight[i] < 0) {
-		pr_append_new_chunk(&lib->error, "Illegal weight in ");
-		goto ERROR;
-	    }
-	    j = INIT_BUF_SIZE;
-	    k = 0;
-	    if(i > 0) {
-		/* We are actually testing the previous sequence. */
-		if(strlen(lib->seqs[i-1]) == 0) {
-		    pr_append_new_chunk(&lib->error, "Empty sequence in ");
-		    goto ERROR;
-		}
-		tmp = upcase_and_check_char(lib->seqs[i-1]);
-		m += tmp;
-		if (tmp && '\0' == offender) offender = tmp;
-	    }
-	    p--;
-	}
-	else {
-	    if(i < 0){ 
-		pr_append_new_chunk(&lib->error,
-				    "Missing id line (expected '>') in ");
-		goto ERROR;
-	    } else {
-		if(k+strlen(p) > j-2){
-		    while(j-2 < k+ strlen(p))j += INIT_BUF_SIZE;
-		    lib->seqs[i] = pr_safe_realloc(lib->seqs[i], j);
-
-		}
-		strcat(lib->seqs[i], p);
-		k += strlen(p);
-	    }
-	}
-    }
-    if(i < 0) {
-	pr_append_new_chunk(&lib->error, "Empty ");
-	goto ERROR;
-    }
-    else if(strlen(lib->seqs[i]) < 3) {
-	pr_append_new_chunk(&lib->error, "Sequence length < 3 in ");
-	goto ERROR;
-    }
-    tmp = upcase_and_check_char(lib->seqs[i]);
-    m += tmp;
-    if (tmp && '\0' == offender) offender = tmp;
-    if (offender) {
-	pr_append_new_chunk(&lib->warning,
-			    "Unrecognized character (");
-	buf[0] = offender;
-	buf[1] = '\0';
-	pr_append(&lib->warning, buf);
-	pr_append(&lib->warning, ") in ");
-	pr_append(&lib->warning, errfrag);
-	pr_append(&lib->warning, " ");
-	pr_append(&lib->warning, lib->repeat_file);
-    }
-    fclose(file);
-    reverse_complement_seq_lib(lib);
-    return;
-
- ERROR:
-    pr_append(&lib->error, errfrag);
-    pr_append(&lib->error, " ");
-    pr_append(&lib->error, lib->repeat_file);
-    if (file) fclose(file);
-}
-
-/* 
- * Free exogenous storage associated with a seq_lib (but not the seq_lib
- * itself).  Silently ignore NULL p.  Set *p to 0 bytes.
- */
-void
-free_seq_lib(p)
-    seq_lib *p;
-{
-    int i;
-    if (NULL == p) return;
-
-    if ( NULL != p->repeat_file) free(p->repeat_file);
-    if (NULL != p->seqs) { 
-	for(i = 0; i < p->seq_num; i++)
-	    if (NULL != p->seqs[i]) free(p->seqs[i]);
-	free(p->seqs);
-    }
-    if (NULL != p->names) {
-	for(i = 0; i < p->seq_num; i++)
-	    if (NULL != p->names[i]) free(p->names[i]);
-	free(p->names);
-    }
-    if (NULL != p->weight) free(p->weight);
-    if (NULL != p->error.data) free(p->error.data);
-    if (NULL != p->warning.data) free(p->warning.data);
-    if (NULL != p->rev_compl_seqs) free(p->rev_compl_seqs);
-    memset(p, 0, sizeof(*p));
-}
-
-/* 
- * Removes spaces and "end-of-line" characters
- * from the sequence, replaces all other
- * characters except A, T, G, C and IUB/IUPAC
- * codes with N.  Returns 0 if there were no such
- * replacements and the first non-ACGT IUB
- * character otherwise. 
- */
-static char
-upcase_and_check_char(s)
-    char *s;
-{
-    int i, j, n, m;
-
-    j = 0; m = 0;
-    n = strlen(s);
-    for(i=0; i<n; i++){
-      
-	switch(s[i])
-	{
-	case 'a' : s[i-j] = 'A'; break;
-	case 'g' : s[i-j] = 'G'; break;
-	case 'c' : s[i-j] = 'C'; break;
-	case 't' : s[i-j] = 'T'; break;
-	case 'n' : s[i-j] = 'N'; break;
-	case 'A' : s[i-j] = 'A'; break;
-	case 'G' : s[i-j] = 'G'; break;
-	case 'C' : s[i-j] = 'C'; break;
-	case 'T' : s[i-j] = 'T'; break;
-	case 'N' : s[i-j] = 'N'; break;
-
-        case 'b' : case 'B': 
-        case 'd' : case 'D':
-        case 'h' : case 'H':
-        case 'v' : case 'V':
-        case 'r' : case 'R':
-        case 'y' : case 'Y':
-        case 'k' : case 'K':
-        case 'm' : case 'M':
-	case 's' : case 'S':
-	case 'w' : case 'W':
-	  s[i-j] = toupper(s[i]); break;
-
-	case '\n': j++;          break;
-	case ' ' : j++;          break;
-	case '\t': j++;          break;
-	case '\r': j++;          break;
-	default  : if (!m) m = s[i]; s[i-j] = 'N'; 
-	}
-    }
-    s[n-j] = '\0';
-    return m;
-}
-
-static double
-parse_seq_name(s)
-char *s;
-{
-    char *p, *q;
-    double n;
-
-    p = s;
-    while( *p != '*' && *p != '\0' ) p++;
-    if (*p == '\0' ) return 1;
-    else {
-	 p++;
-	 n = strtod( p, &q );
-	 if( q == p ) return -1;
-    }
-    if(n > PR_MAX_LIBRARY_WT) return -1;
-
-    return n;
-}
-
-static void
-reverse_complement_seq_lib(lib)
-seq_lib  *lib;
-{
-    int i, n, k;
-    if((n = lib->seq_num) == 0) return;
-    else {
-	lib->names = pr_safe_realloc(lib->names, 2*n*sizeof(*lib->names));
-	lib->seqs = pr_safe_realloc(lib->seqs, 2*n*sizeof(*lib->seqs));
-	lib->weight = pr_safe_realloc(lib->weight, 2*n*sizeof(*lib->weight));
-	lib->rev_compl_seqs = pr_safe_malloc(2*n*sizeof(*lib->seqs));
-
-	lib->seq_num *= 2;
-	for(i=n; i<lib->seq_num; i++){
-	    k = strlen(lib->names[i-n]);
-	    lib->names[i] = pr_safe_malloc(k + 9);
-	    strcpy(lib->names[i], "reverse ");
-	    strcat(lib->names[i], lib->names[i-n]);
-	    lib->seqs[i] = pr_safe_malloc(strlen(lib->seqs[i-n]) + 1);
-	    _pr_reverse_complement(lib->seqs[i-n], lib->seqs[i]);
-	    lib->weight[i] = lib->weight[i-n];
-	    lib->rev_compl_seqs[i-n] = lib->seqs[i];
-	    lib->rev_compl_seqs[i] = lib->seqs[i-n];
-       }
-    }
-    return;
-}
-
-#endif
-
 static int
 parse_seq_quality(s, num)
    char *s;
@@ -1049,7 +797,7 @@ parse_seq_quality(s, num)
 
    p = q = s;
    k = strlen(s);
-   g = *num = pr_safe_malloc(sizeof(int)*k);
+   g = *num = _rb_safe_malloc(sizeof(int)*k);
    while(*p == ' ' || *p == '\t'){
       p++;
       if(*p == '\0' || *p == '\n') return 0;
@@ -1064,3 +812,32 @@ parse_seq_quality(s, num)
    }
    return i;
 }
+
+/* =========================================================== */
+/* Fail-stop wrappers for memory allocation.                   */
+/* =========================================================== */
+/* 
+ * Panic messages for when the program runs out of memory.
+ */
+#define OOM_MESSAGE      "out of memory in read_boulder\n"
+#define OOM_MESSAGE_LEN  30
+#define OOM_ERROR write(2, OOM_MESSAGE, OOM_MESSAGE_LEN), exit(-2)
+
+static void *
+_rb_safe_malloc(size_t x)
+{
+    void *r = malloc(x);
+    if (NULL == r) OOM_ERROR;
+    return r;
+}
+
+static void *
+_rb_safe_realloc(void *p, size_t x)
+{
+    void *r = realloc(p, x);
+    if (NULL == r) OOM_ERROR;
+    return r;
+}
+
+/* End of fail-stop wrappers. */
+/* =========================================================== */
