3506c80a98cdcb7385071de30d1eb220cf4a4a70
Steven G. Rozen <steve@steverozen.net>
2007-10-26 15:44:43 +0000

    minor updates

--- primer3_boulder_main.c
+++ primer3_boulder_main.c
@@ -44,42 +44,6 @@ static void   sig_handler(int);
 
 /* FIX ME -- deal with program name. */
 
-/* Global static variables. */
-static const char * copyright[] = {
-"",
-"Copyright (c) 1996,1997,1998,1999,2000,2001,2004,2006,2007",
-"Whitehead Institute for Biomedical Research, Steve Rozen",
-"(http://jura.wi.mit.edu/rozen), and Helen Skaletsky",
-"All rights reserved.",
-"",
-"Redistribution and use in source and binary forms, with or without",
-"modification, are permitted provided that the following conditions are",
-"met:",
-"",
-"   * Redistributions of source code must retain the above copyright",
-"notice, this list of conditions and the following disclaimer.",
-"   * Redistributions in binary form must reproduce the above",
-"copyright notice, this list of conditions and the following disclaimer",
-"in the documentation and/or other materials provided with the",
-"distribution.",
-"   * Neither the names of the copyright holders nor contributors may",
-"be used to endorse or promote products derived from this software",
-"without specific prior written permission.",
-"",
-"THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS",
-"\"AS IS\" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT",
-"LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR",
-"A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT",
-"OWNERS OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,",
-"SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT",
-"LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,",
-"DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY",
-"THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT",
-"(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE",
-"OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.",
-NULL
-};
-
 /* Other global variables. */
 static const char * pr_release = "primer3 release 1.1.2";
 static const char *pr_program_name;
@@ -229,8 +193,7 @@ main(argc,argv)
 static void
 print_usage()
 {
-    const char **p;
-    p = &copyright[0];
+    const char **p = libprimer3_copyright();
     while (NULL != *p) fprintf(stderr, "%s\n", *p++);
     fprintf(stderr, 
 	    "\n\nUSAGE: %s %s %s\n", pr_program_name,
