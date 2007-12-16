a9a8a5355631161f08c6fe957e8b6fde93952fa2
Andreas Untergasser <andreas@untergasser.de>
2007-12-16 07:24:25 +0000

    Added comments, moved code

--- src/libprimer3.c
+++ src/libprimer3.c
@@ -465,11 +465,17 @@ overlap the 3'-end of primer.
 #define PAIR_WT_PRODUCT_SIZE_LT     0
 #define PAIR_WT_PRODUCT_SIZE_GT     0
 
+/* Set the program name */
 void
 p3_set_program_name(const char *pname) {
   pr_program_name = pname;
 }
 
+/* ============================================================ */
+/* BEGIN functions for global settings                          */
+/* ============================================================ */
+
+/* Allocate space for global settings and fill in defaults */
 p3_global_settings *
 p3_create_global_settings() {
   p3_global_settings *r;
@@ -483,6 +489,7 @@ p3_create_global_settings() {
   return r;
 }
 
+/* Free the space of global settings */
 void 
 p3_destroy_global_settings(p3_global_settings *a) {
   if (NULL != a) {
@@ -492,6 +499,7 @@ p3_destroy_global_settings(p3_global_settings *a) {
   }
 }
 
+/* Write the default values in global settings */
 void
 pr_set_default_global_args(p3_global_settings *a) {
     memset(a, 0, sizeof(*a));  
@@ -621,6 +629,7 @@ pr_set_default_global_args(p3_global_settings *a) {
     a->lib_ambiguity_codes_consensus   = LIB_AMBIGUITY_CODES_CONSENSUS;
 }
 
+/* Add a valuepair to the array of intervals */
 int
 p3_add_to_interval_array(interval_array_t2 *interval_arr, int i1, int i2) 
 {
@@ -633,13 +642,18 @@ p3_add_to_interval_array(interval_array_t2 *interval_arr, int i1, int i2)
 }
 
 /* ============================================================ */
+/* END functions for global settings                            */
+/* ============================================================ */
+
+
+
+/* ============================================================ */
 /* BEGIN functions for p3retval                                 */
 /* ============================================================ */
 
 /* Allocate a new primer3 state. Return NULL if out of memory. Assuming
    malloc sets errno to ENOMEM according to Unix98, set errno to ENOMEM
    on out-of-memory error. */
-
 static p3retval *
 create_p3retval(void)
 {
@@ -750,6 +764,7 @@ p3_get_retval_output_type(const p3retval *r) {
 /* BEGIN functions for dpal_arg_holder                          */
 /* ============================================================ */
 
+/* Create the dpal arg holder */
 dpal_arg_holder *
 create_dpal_arg_holder () {
 
@@ -778,6 +793,7 @@ create_dpal_arg_holder () {
   return h;
 }
 
+/* Free the dpal arg holder */
 void
 destroy_dpal_arg_holder(dpal_arg_holder *h) {
   free(h->local);
@@ -788,10 +804,17 @@ destroy_dpal_arg_holder(dpal_arg_holder *h) {
   free(h);
 }
 
+/* FIX ME AU: Dont understand what this is good for */
+static dpal_arg_holder *dpal_arg_to_use = NULL;
+
 /* ============================================================ */
 /* END functions for dpal_arg_holder                            */
 /* ============================================================ */
 
+
+/* ============================================================ */
+/* BEGIN functions for seq_arg_holder                           */
+/* ============================================================ */
 
 /* Create and initialize a seq_args data structure */
 seq_args
@@ -808,6 +831,7 @@ seq_args
   return r;
 }
 
+/* Free a seq_arg data structure */
 void
 destroy_seq_args(seq_args *sa) {
   if (NULL != sa->internal_input) free(sa->internal_input);
@@ -828,12 +852,16 @@ destroy_seq_args(seq_args *sa) {
   free(sa);
 }
 
-static dpal_arg_holder *dpal_arg_to_use = NULL;
+/* ============================================================ */
+/* END functions for seq_arg_holder                             */
+/* ============================================================ */
 
-/* ================================================================== */
-/* The main primer3 interface */
 
-/* See libprimer3.h for documentation. */
+/* ============================================================ */
+/* The main primer3 interface                                   */
+/* See libprimer3.h for documentation.                          */
+/* ============================================================ */
+
 p3retval *
 choose_primers(const p3_global_settings *pa, seq_args *sa)
 {
@@ -1920,212 +1948,6 @@ oligo_max_template_mispriming(h)
   return h->template_mispriming > h->template_mispriming_r ?
     h->template_mispriming : h->template_mispriming_r;
 }
-
-
-/* START OF WHAT SHOULD GO TO IO_PRIMER_FILES.C */
-
-/* return 0 on success, 1 on error */
-/* Check errno for ENOMEM */
-/* This function is for backward compatability
-   in boulder io testing. */
-int
-p3_print_oligo_lists(const p3retval *retval,
-		     const seq_args *sa,
-		     const p3_global_settings *pa,
-		     pr_append_str *err)
-{
-  /* Figure out if the sequence starts at position 1 or 0 */
-  int   first_base_index = pa->first_base_index;
-  int   ret;
-  /* Start building up a filename */
-  char *file;
-  FILE *fh;
-
-  if (setjmp(_jmp_buf) != 0) {
-    return 1;  /* If we get here, that means we returned via a longjmp.
-		  In this case errno should be ENOMEM. */
-
-  }
-
-  file = malloc(strlen(sa->sequence_name) + 5);
-  if (NULL == file) return 1; /* ENOMEM */
-
-  /* Check if the left primers have to be printed */
-  /* OK pa->primer_task != pick_right_only 
-     && pa->primer_task != pick_hyb_probe_only*/
-  if( pa->pick_left_primer ) {
-    /* Create the file name and open file*/
-    strcpy(file, sa->sequence_name);
-    strcat(file, ".for");
-    if (!(fh = fopen(file,"w"))) {
-      if (pr_append_new_chunk_external(err, "Unable to open file "))
-	return 1;
-      if (pr_append_external(err, file)) return 1;
-      if (pr_append_external(err, " for writing")) return 1;
-      free(file);
-      return 1;
-    }
-
-    /* Print the content to the file */
-    ret = p3_print_one_oligo_list(sa, retval->fwd.num_elem,
-    			  retval->fwd.oligo, OT_LEFT, 
-    			  first_base_index, NULL != pa->p_args.repeat_lib, fh);
-    fclose(fh);
-    if (ret) return 1;
-  }
-
-  /* Check if the right primers have to be printed */
-  if (pa->pick_right_primer 
-      /* pa->primer_task != pick_left_only 
-	 && pa->primer_task != pick_hyb_probe_only*/ ) {
-    strcpy(file, sa->sequence_name);
-    strcat(file, ".rev");
-    if (!(fh = fopen(file,"w"))) {
-      pr_append_new_chunk(err, "Unable to open file ");
-      pr_append(err, file);
-      pr_append(err, " for writing");
-      free(file);
-      return 1;
-    }
-    /* Print the content to the file */
-    ret = p3_print_one_oligo_list(sa, retval->rev.num_elem,
-    			  retval->rev.oligo, OT_RIGHT,
-    			  first_base_index, NULL != pa->p_args.repeat_lib, fh);
-
-    fclose(fh);
-    if (ret) return 1;
-  }
-
-  /* Check if the internal oligos have to be printed */
-  if (pa->pick_internal_oligo) { 
-    /* pa->primer_task == pick_pcr_primers_and_hyb_probe 
-       || pa->primer_task == pick_hyb_probe_only */ 
-    /* Create the file name and open file*/
-    strcpy(file, sa->sequence_name);
-    strcat(file, ".int");
-    if (!(fh = fopen(file,"w"))) {
-      if (pr_append_new_chunk_external(err, "Unable to open file "))
-	return 1;
-      if (pr_append_external(err, file)) return 1;
-      if (pr_append_external(err, " for writing")) return 1;
-      free(file);
-      return 1;
-    }
-    /* Print the content to the file */
-    ret = p3_print_one_oligo_list(sa, retval->intl.num_elem,
-    			  retval->intl.oligo, OT_INTL,
-				  first_base_index, NULL != pa->o_args.repeat_lib, fh);
-    fclose(fh);
-    if (ret) return 1;
-  }
-  free(file);
-  return 0;
-}
-
-/* Print out the content of one primer array */
-/* Return 1 on error, otherwise 0. */
-int
-p3_print_one_oligo_list(const seq_args *sa,
-			int n,
-			const primer_rec *oligo_arr,
-			const oligo_type o_type,
-			const int first_base_index, 
-			const int print_lib_sim,
-			FILE  *fh
-			)
-{
-  int i;
-
-    /* Print out the header for the table */
-    if (print_list_header(fh, o_type, first_base_index, print_lib_sim))
-      return 1; /* error */
-    /* Iterate over the array */
-    for (i = 0; i < n; i++) {
-    /* Print each single oligo */
-	if (print_oligo(fh, sa, i, &oligo_arr[i], o_type,
-			first_base_index, print_lib_sim))
-	  return 1; /* error */
-    }
-    return 0; /* success */
-}
-
-static int
-print_list_header(fh, type, first_base_index, print_lib_sim)
-    FILE *fh;
-    oligo_type type;
-    int first_base_index, print_lib_sim;
-{
-  int ret;
-    ret = fprintf(fh, "ACCEPTABLE %s\n",
-            OT_LEFT == type ? "LEFT PRIMERS"
-            : OT_RIGHT == type ? "RIGHT PRIMERS" : "INTERNAL OLIGOS");
-    if (ret < 0) return 1;
-
-    ret = fprintf(fh, "                               %4d-based     ", 
-	    first_base_index);
-    if (ret < 0) return 1;
-
-    if (print_lib_sim)
-	ret = fprintf(fh, "#               self  self   lib  qual-\n");
-    else
-	ret = fprintf(fh, "#               self  self  qual-\n");
-    if (ret < 0) return 1;    
-
-    ret = fprintf(fh, "   # sequence                       start ln  "); 
-    if (ret < 0) return 1;
-
-    if (print_lib_sim)
-	ret = fprintf(fh, "N   GC%%     Tm   any   end   sim   lity\n");
-    else 
-	ret = fprintf(fh, "N   GC%%     Tm   any   end   lity\n");
-
-    if (ret < 0) return 1;
-    return 0;
-}
-
-static int
-print_oligo(FILE *fh,
-	    const seq_args *sa, 
-	    int index, 
-	    const primer_rec *h, 
-	    oligo_type type, 
-	    int first_base_index, 
-	    int print_lib_sim)
-{
-  int ret;
-    char *p =  /* WARNING, *p points to static storage that
-		  is overwritten on next call to pr_oligo_sequence
-		  or pr_oligo_rev_c_sequence. */
-      (OT_RIGHT != type) 
-      ? pr_oligo_sequence(sa, h) 
-      : pr_oligo_rev_c_sequence(sa, h);
-
-    if (print_lib_sim) {
-	ret = fprintf(fh,
-		"%4d %-30s %5d %2d %2d %5.2f %5.3f %5.2f %5.2f %5.2f %6.3f\n",
-		index, p, h->start+sa->incl_s + first_base_index, 
-		h->length,
-		h->num_ns, h->gc_content, h->temp,
-		h->self_any / PR_ALIGN_SCORE_PRECISION,
-		h->self_end / PR_ALIGN_SCORE_PRECISION,
-		h->repeat_sim.score[h->repeat_sim.max] /PR_ALIGN_SCORE_PRECISION,
-		h->quality);
-    } else {
-	ret = fprintf(fh,
-		"%4d %-30s %5d %2d %2d %5.2f %5.3f %5.2f %5.2f %6.3f\n",
-		index, p, h->start+sa->incl_s + first_base_index, 
-		h->length,
-		h->num_ns, h->gc_content, h->temp,
-		h->self_any / PR_ALIGN_SCORE_PRECISION,
-		h->self_end / PR_ALIGN_SCORE_PRECISION,
-		h->quality);
-    }
-    if (ret < 0) return 1;
-    else return 0;
-}
-
-/* END OF WHAT SHOULD GO TO IO_PRIMER_FILES.C */
-
 
 
 /* This function requires that retval->fwd.num_elem and rev.num_elem,
@@ -4095,7 +3917,7 @@ libprimer3_copyright(void) {
 }
 
 
-
+
 /* ============================================================ */
 
 /* Used in seq_lib functions and also exported */
@@ -4157,7 +3979,222 @@ FILE *file;
     }
 }
 
-
+
+/* ============================================================ */
+/* BEGIN create primer files functions                          */
+/* ============================================================ */
+
+/* START OF WHAT SHOULD GO TO IO_PRIMER_FILES.C */
+
+/* return 0 on success, 1 on error */
+/* Check errno for ENOMEM */
+/* This function is for backward compatability
+   in boulder io testing. */
+int
+p3_print_oligo_lists(const p3retval *retval,
+		     const seq_args *sa,
+		     const p3_global_settings *pa,
+		     pr_append_str *err)
+{
+  /* Figure out if the sequence starts at position 1 or 0 */
+  int   first_base_index = pa->first_base_index;
+  int   ret;
+  /* Start building up a filename */
+  char *file;
+  FILE *fh;
+
+  if (setjmp(_jmp_buf) != 0) {
+    return 1;  /* If we get here, that means we returned via a longjmp.
+		  In this case errno should be ENOMEM. */
+
+  }
+
+  file = malloc(strlen(sa->sequence_name) + 5);
+  if (NULL == file) return 1; /* ENOMEM */
+
+  /* Check if the left primers have to be printed */
+  /* OK pa->primer_task != pick_right_only 
+     && pa->primer_task != pick_hyb_probe_only*/
+  if( pa->pick_left_primer ) {
+    /* Create the file name and open file*/
+    strcpy(file, sa->sequence_name);
+    strcat(file, ".for");
+    if (!(fh = fopen(file,"w"))) {
+      if (pr_append_new_chunk_external(err, "Unable to open file "))
+	return 1;
+      if (pr_append_external(err, file)) return 1;
+      if (pr_append_external(err, " for writing")) return 1;
+      free(file);
+      return 1;
+    }
+
+    /* Print the content to the file */
+    ret = p3_print_one_oligo_list(sa, retval->fwd.num_elem,
+    			  retval->fwd.oligo, OT_LEFT, 
+    			  first_base_index, NULL != pa->p_args.repeat_lib, fh);
+    fclose(fh);
+    if (ret) return 1;
+  }
+
+  /* Check if the right primers have to be printed */
+  if (pa->pick_right_primer 
+      /* pa->primer_task != pick_left_only 
+	 && pa->primer_task != pick_hyb_probe_only*/ ) {
+    strcpy(file, sa->sequence_name);
+    strcat(file, ".rev");
+    if (!(fh = fopen(file,"w"))) {
+      pr_append_new_chunk(err, "Unable to open file ");
+      pr_append(err, file);
+      pr_append(err, " for writing");
+      free(file);
+      return 1;
+    }
+    /* Print the content to the file */
+    ret = p3_print_one_oligo_list(sa, retval->rev.num_elem,
+    			  retval->rev.oligo, OT_RIGHT,
+    			  first_base_index, NULL != pa->p_args.repeat_lib, fh);
+
+    fclose(fh);
+    if (ret) return 1;
+  }
+
+  /* Check if the internal oligos have to be printed */
+  if (pa->pick_internal_oligo) { 
+    /* pa->primer_task == pick_pcr_primers_and_hyb_probe 
+       || pa->primer_task == pick_hyb_probe_only */ 
+    /* Create the file name and open file*/
+    strcpy(file, sa->sequence_name);
+    strcat(file, ".int");
+    if (!(fh = fopen(file,"w"))) {
+      if (pr_append_new_chunk_external(err, "Unable to open file "))
+	return 1;
+      if (pr_append_external(err, file)) return 1;
+      if (pr_append_external(err, " for writing")) return 1;
+      free(file);
+      return 1;
+    }
+    /* Print the content to the file */
+    ret = p3_print_one_oligo_list(sa, retval->intl.num_elem,
+    			  retval->intl.oligo, OT_INTL,
+				  first_base_index, NULL != pa->o_args.repeat_lib, fh);
+    fclose(fh);
+    if (ret) return 1;
+  }
+  free(file);
+  return 0;
+}
+
+/* Print out the content of one primer array */
+/* Return 1 on error, otherwise 0. */
+int
+p3_print_one_oligo_list(const seq_args *sa,
+			int n,
+			const primer_rec *oligo_arr,
+			const oligo_type o_type,
+			const int first_base_index, 
+			const int print_lib_sim,
+			FILE  *fh
+			)
+{
+  int i;
+
+    /* Print out the header for the table */
+    if (print_list_header(fh, o_type, first_base_index, print_lib_sim))
+      return 1; /* error */
+    /* Iterate over the array */
+    for (i = 0; i < n; i++) {
+    /* Print each single oligo */
+	if (print_oligo(fh, sa, i, &oligo_arr[i], o_type,
+			first_base_index, print_lib_sim))
+	  return 1; /* error */
+    }
+    return 0; /* success */
+}
+
+static int
+print_list_header(fh, type, first_base_index, print_lib_sim)
+    FILE *fh;
+    oligo_type type;
+    int first_base_index, print_lib_sim;
+{
+  int ret;
+    ret = fprintf(fh, "ACCEPTABLE %s\n",
+            OT_LEFT == type ? "LEFT PRIMERS"
+            : OT_RIGHT == type ? "RIGHT PRIMERS" : "INTERNAL OLIGOS");
+    if (ret < 0) return 1;
+
+    ret = fprintf(fh, "                               %4d-based     ", 
+	    first_base_index);
+    if (ret < 0) return 1;
+
+    if (print_lib_sim)
+	ret = fprintf(fh, "#               self  self   lib  qual-\n");
+    else
+	ret = fprintf(fh, "#               self  self  qual-\n");
+    if (ret < 0) return 1;    
+
+    ret = fprintf(fh, "   # sequence                       start ln  "); 
+    if (ret < 0) return 1;
+
+    if (print_lib_sim)
+	ret = fprintf(fh, "N   GC%%     Tm   any   end   sim   lity\n");
+    else 
+	ret = fprintf(fh, "N   GC%%     Tm   any   end   lity\n");
+
+    if (ret < 0) return 1;
+    return 0;
+}
+
+static int
+print_oligo(FILE *fh,
+	    const seq_args *sa, 
+	    int index, 
+	    const primer_rec *h, 
+	    oligo_type type, 
+	    int first_base_index, 
+	    int print_lib_sim)
+{
+  int ret;
+    char *p =  /* WARNING, *p points to static storage that
+		  is overwritten on next call to pr_oligo_sequence
+		  or pr_oligo_rev_c_sequence. */
+      (OT_RIGHT != type) 
+      ? pr_oligo_sequence(sa, h) 
+      : pr_oligo_rev_c_sequence(sa, h);
+
+    if (print_lib_sim) {
+	ret = fprintf(fh,
+		"%4d %-30s %5d %2d %2d %5.2f %5.3f %5.2f %5.2f %5.2f %6.3f\n",
+		index, p, h->start+sa->incl_s + first_base_index, 
+		h->length,
+		h->num_ns, h->gc_content, h->temp,
+		h->self_any / PR_ALIGN_SCORE_PRECISION,
+		h->self_end / PR_ALIGN_SCORE_PRECISION,
+		h->repeat_sim.score[h->repeat_sim.max] /PR_ALIGN_SCORE_PRECISION,
+		h->quality);
+    } else {
+	ret = fprintf(fh,
+		"%4d %-30s %5d %2d %2d %5.2f %5.3f %5.2f %5.2f %6.3f\n",
+		index, p, h->start+sa->incl_s + first_base_index, 
+		h->length,
+		h->num_ns, h->gc_content, h->temp,
+		h->self_any / PR_ALIGN_SCORE_PRECISION,
+		h->self_end / PR_ALIGN_SCORE_PRECISION,
+		h->quality);
+    }
+    if (ret < 0) return 1;
+    else return 0;
+}
+
+/* END OF WHAT SHOULD GO TO IO_PRIMER_FILES.C */
+
+
+/* ============================================================ */
+/* END create primer files functions                            */
+/* ============================================================ */
+
+
+
 /* ============================================================ */
 /* BEGIN seq_lib functions                                      */
 /* ============================================================ */
@@ -4466,7 +4503,7 @@ p3_get_sa_excl_internal2(seq_args *sargs) {
 /* END 'get' functions for seq_args                             */
 /* ============================================================ */
 
-
+
 /* ============================================================ */
 /* BEGIN 'set' functions for seq_args                           */
 /* ============================================================ */
