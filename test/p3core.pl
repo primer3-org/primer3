f1e52fc36d2941afe0da95a7951d7d8371da7d05
Steven G. Rozen <steve@steverozen.net>
2008-01-15 00:16:38 +0000

    added copyright to p3core

--- test/p3core.pl
+++ test/p3core.pl
@@ -1,5 +1,49 @@
 #!/usr/bin/perl
 
+# Approximate perl re-implementation of the primer3_core executable
+# for testing.
+#
+# For usage, see the usage statement in the code, below.
+#
+# ======================================================================
+# (c) Copyright 1996,1997,1998,1999,2000,2001,2004,2006,2007,2008
+#
+# Whitehead Institute for Biomedical Research, Steve Rozen, and Helen
+# Skaletsky
+#
+# All rights reserved.
+# 
+#   This file is part of the primer3 suite.
+#
+#   The primer3 suite is free software; you can
+#   redistribute them and/or modify them under the terms of the GNU
+#   General Public License as published by the Free Software Foundation;
+#   either version 2 of the License, or (at your option) any later
+#   version.
+#
+#   This software is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with this file (file gpl-2.0.txt in the source distribution); if
+#   not, write to the Free Software Foundation, Inc., 51 Franklin St,
+#   Fifth Floor, Boston, MA 02110-1301 USA
+# 
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+# A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+# OWNERS OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+# SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+# LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+# DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+# THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+# ======================================================================
+
 use warnings 'all';
 use strict;
 
