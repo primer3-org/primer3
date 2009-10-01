9d5750f4addc266c56a9128ae572779a6ced8e87
Steven G. Rozen <steve@steverozen.net>
2009-10-01 00:12:53 +0000

    tweaks to testing and release how-to

--- src/paranoid_tests.sh
+++ src/paranoid_tests.sh
@@ -6,6 +6,9 @@ echo; echo; echo 'STARTING 2ND SET OF TESTS (-O3)'; echo; echo;
 make O_OPTS=-O3
 make test
 make clean
-echo; echo; echo 'STARTING 3RD SET OF TESTS (-O0) and valgrind'; echo; echo;
+echo; echo; echo 'STARTING 3RD SET OF TESTS (-O0 and valgrind)'; echo; echo;
 make O_OPTS=-O0
+cd ../test; make VAL=--valgrind
+echo; echo; echo 'STARTING 4TH SET OF TESTS (-O0, -io_verison=3, and valgrind)'; echo; echo;
+cd io_version_3
 cd ../test; make VAL=--valgrind
