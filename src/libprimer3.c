0bdd10a699bd61c64eab00d9e4bffadb67a5984f
Ioana Cutcutache <ioana.cutcutache@gmail.com>
2010-03-09 07:23:40 +0000

    updated release number

--- src/libprimer3.c
+++ src/libprimer3.c
@@ -5057,7 +5057,7 @@ p3_get_pair_array_explain_string(const pair_array_t *pair_array)
 const char *
 libprimer3_release(void) 
 {
-  return "libprimer3 release 2.2.1";
+  return "libprimer3 release 2.2.2";
 }
 
 const char *
