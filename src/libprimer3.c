3671f4ac690590e8c8ed443ccede14e6c0743765
Steven G. Rozen <steve@steverozen.net>
2012-05-18 02:21:45 +0000

    updated release number

--- src/libprimer3.c
+++ src/libprimer3.c
@@ -5328,7 +5328,7 @@ p3_get_pair_array_explain_string(const pair_array_t *pair_array)
 const char *
 libprimer3_release(void) 
 {
-  return "libprimer3 release 2.3.2";
+  return "libprimer3 release 2.3.3";
 }
 
 const char *
