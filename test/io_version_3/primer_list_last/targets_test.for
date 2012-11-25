14183c1c36056c5a04f72bb29010d9f6e00dea5c
Ioana Cutcutache <ioana.cutcutache@gmail.com>
2012-11-25 18:46:14 +0000

    removed io version 3 tests and updated some outputs based on new updated usage line

--- test/io_version_3/primer_list_last/targets_test.for
+++
@@ -1,6 +0,0 @@
-ACCEPTABLE LEFT PRIMERS
-                                  0-based     #               self  self  qual-
-   # sequence                       start ln  N   GC%     Tm   any   end   lity
-   0 TCCTACAGCTGTGGGAAAATG              0 21  0 47.62 60.118 12.00  0.00  1.118
-   1 CCTACAGCTGTGGGAAAATG               1 20  0 50.00 58.228 10.00  0.00  1.772
-   2 TCCTACAGCTGTGGGAAAAT               0 20  0 45.00 57.268 12.00  2.00  2.732
