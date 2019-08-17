43c33e474ba4e70567b72486037feca669eee28f
Andreas Untergasser <andreas@untergasser.de>
2019-08-17 05:54:30 +0200

    Updated version for release 2.5.0

--- test/p3test.pl
+++ test/p3test.pl
@@ -94,7 +94,7 @@ sub main() {
     # select STDERR;
     $| = 1;
 
-    print "$0 for primer3_core version 2.4.0\n";
+    print "$0 for primer3_core version 2.5.0\n";
     
     $all_ok = 1;
     my $overall_start_time = time();
