7f9f17d6012f404e83bbf0f931a4d06eb4af465b
Andreas Untergasser <andreas@untergasser.de>
2022-01-26 04:50:57 +0100

    Updated version for release v2.6.1

--- src/libprimer3.cc
+++ src/libprimer3.cc
@@ -6279,7 +6279,7 @@ p3_get_pair_array_explain_string(const pair_array_t *pair_array)
 const char *
 libprimer3_release(void) 
 {
-  return "libprimer3 release 2.6.0";
+  return "libprimer3 release 2.6.1";
 }
 
 const char *
